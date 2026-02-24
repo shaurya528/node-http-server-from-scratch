43611a025162f74ce2b616ad700d1c5af890ff2f
shaurya528 <rajshaurya258@gmail.com>
2026-02-24 17:02:40 +0530

    completed basic http server project

--- BasicnodeServer/node.js
+++ BasicnodeServer/node.js
@@ -1,8 +1,35 @@
 import http from "http"
 const PORT =3002;
+const Data = {User:["prince ","kavya","cutie"]}
 const server = http.createServer((req,res)=>{
-if(req.url=="/"){
-    res.end("working")
+if(req.url=="/" && req.method=="GET"){
+    res.writeHead(200,{'content-type':'application/json'})
+     res.end(JSON.stringify(Data))
+    
+}else if(req.url=="/push" && req.method=="POST"){
+    let body="";
+    req.on('data',chunk=>{
+          body+=chunk;
+    })
+    req.on('end',()=>{
+        try{
+            let parsed= JSON.parse(body);
+                if(!parsed.name){
+                    res.writeHead(400, { "Content-Type": "application/json" });
+                    res.end( JSON.stringify({message:"username is required"}));
+                }
+                Data.User.push(parsed.name);
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end( JSON.stringify({message:"username succes saved",user:Data.User}));
+        }catch(err){
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Invalid JSON" }));
+        }
+    })
+   
+}else{
+    res.writeHead(404,{'content-type':'application/json'});
+    res.end(JSON.stringify({message:"unable to detch your request"}))
 }
 
 })
